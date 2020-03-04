981b4a9baed3b0fa43c8bdfb23fcd334da1c1512
ScottCho <scottcho@126.com>
2020-03-04 11:23:03 +0800

    app增加name

--- app/apis/v2/schemas/vcs.py
+++ app/apis/v2/schemas/vcs.py
@@ -29,7 +29,7 @@ class BaselineSchema(Schema):
     status_id = fields.Integer()
     status_name = fields.Function(lambda obj: "{}".format(obj.status.status))
     package_name = fields.Function(lambda obj: "{}".format(obj.package.name))
-    app_name = fields.Function(lambda obj: "{}-{}-{}".format(obj.app.project.name.lower(),obj.app.env.name.lower(),obj.app.subsystem.en_name.lower()))
+    app_name = fields.Function(lambda obj: "{}".format(obj.app.name))
     developer_username = fields.Function(lambda obj: "{}".format(obj.developer.username))
     developer = Relationship(self_view='baseline_developer',
                              self_view_kwargs={'id': '<id>'},
