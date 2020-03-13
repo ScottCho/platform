f5e93a927dda6c8bfde5db9cc11d304e9c2dc663
ScottCho <scottcho@126.com>
2020-03-13 15:56:33 +0800

    用户添加批量删除接口

--- app/apis/v2/resources/auth.py
+++ app/apis/v2/resources/auth.py
@@ -265,6 +265,17 @@ class UserList(ResourceList):
 
 class UserDetail(ResourceDetail):
     decorators = (auth_required,)
+    # 改写成批量删除，kwargs={'id':'[1,2,3]'}或者 kwargs={'id':1}
+    # 支持两种方式删除
+    def delete_object(self, kwargs):
+        ids = kwargs.get('id')
+        if ids[0] != '[':
+            obj = self._data_layer.get_object(kwargs)
+            self._data_layer.delete_object(obj, kwargs)
+        else:
+            for id in ids[1:-1].split(','):
+                obj = self._data_layer.get_object({'id':id})
+                self._data_layer.delete_object(obj, {'id':id})
     schema = UserSchema
     data_layer = {'session': db.session,
                   'model': User}
@@ -302,7 +313,7 @@ api.route(ProjectRelationship, 'project_users', '/api/projects/<int:id>/relation
 api.route(ProjectRelationship, 'project_apps', '/api/projects/<int:id>/relationships/apps')
 #用户
 api.route(UserList, 'user_list', '/api/users')
-api.route(UserDetail, 'user_detail', '/api/users/<int:id>')
+api.route(UserDetail, 'user_detail', '/api/users/<id>')
 api.route(UserRelationship, 'user_projects', '/api/users/<int:id>/relationships/projects')
 api.route(UserRelationship, 'user_role', '/api/users/<int:id>/relationships/role')
 
