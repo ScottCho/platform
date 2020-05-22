8b1888ba9e44e833cba020b660b1676bf936f252
scott <scottcho@qq.com>
2020-05-22 14:07:41 +0800

    fix bug

--- app/utils/svn.py
+++ app/utils/svn.py
@@ -10,6 +10,6 @@ import svn.remote
 def  diff_summary_files(svn_source_dir,start_version,end_version):
     l = svn.remote.RemoteClient(svn_source_dir)
     source_file_list = l.diff_summary(start_version,end_version)   
-    #转换成编译后的路径,只增加修改和删除的路径,不包括目录
+    #转换成编译后的路径,只增加修改和增加的路径,不包括目录
     source_files = [f.get('path') for f in source_file_list if f.get('item')=='modified' or f.get('item')=='added' and f.get('kind')=='file']
     return source_files
